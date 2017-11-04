19945a4a5561ac82cd2434e448bc57f8f70bbf21
MatthewWEdwards <mathew41796@gmail.com>
2017-11-04 00:22:27 -0500

    Moved some files around

---
+++ mesh_test/get_obj_from_image.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Initialize chrome webdriver
+options = webdriver.ChromeOptions() 
+prefs = {'download.default_directory' : 'C:\Users\Matthew Edwards\Desktop\ee371r_proj\mesh_test\downloads'}
+options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome('C:\Users\Matthew Edwards\Desktop\ee371r_proj\mesh_test\chromedriver.exe', 
+                          chrome_options=options)
+driver.get("http://cvl-demos.cs.nott.ac.uk/vrn/")
+
+# Upload image
+upload = driver.find_element_by_name("fileToUpload")
+upload.send_keys("C:\Users\Matthew Edwards\Desktop\ee371r_proj\mesh_test\matt.jpg")
+driver.find_element_by_name("submit").click()
+
+# Grab download
+wait = WebDriverWait(driver, 30)
+wait.until(EC.title_is("3D Reconstruction from a Single Image"))
+time.sleep(5) # TODO: Experiment with this wait time. Waiting is definitely necessary.
+obj_download = driver.find_element_by_xpath("//*[@id=\"objlink\"]")
+download = obj_download.click()
+
+time.sleep(20) # TODO: Experiment with this wait time. Waiting is definitely necessary.
+
+driver.close()
+driver.quit()
