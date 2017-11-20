c0894bb180bb26e8b6f6982d33d660570c8d8907
javiertzepeda <javier.t.zepeda@utexas.edu>
2017-11-20 15:34:23 -0600

    added obj face transformation tool.

--- mesh_test/test.m
+++ mesh_test/test.m
@@ -2,6 +2,17 @@ close all
 clear
 
 error_mes = python('get_obj_from_image.py');
-[V,C,F] = read_vertices_and_faces_from_obj_file('downloads/a14b922f8c771675dcd5fda8746c8e1d.obj');
+[V,C,F] = read_vertices_and_faces_from_obj_file('downloads/matt.obj');
 trisurf(F,V(:,1),V(:,2),V(:,3),'FaceVertexCData', C, 'edgecolor','none');
+ 
 
+
+figure;
+az = 0; el = -40;
+A = viewmtx(az, el);
+vdim4 = [V ones(size(V,1), 1)];
+vdim4 = vdim4';
+viewV = A*vdim4;
+viewV = viewV';
+p = patch('Faces', F, 'Vertices', viewV(:,1:2),'FaceVertexCData', C,...
+    'FaceColor', 'flat', 'edgecolor','none');
